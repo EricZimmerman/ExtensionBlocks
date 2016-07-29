0c4a3b7d4e9e5597dc5aca6b61ee0b3d4d3e7d74
Eric Zimmerman <saericzimmerman@gmail.com>
2016-07-29 13:21:55 -0400

    add test project

---
+++ ExtensionBlocks.Test/TestMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ExtensionBlocks.Test
+{
+    [TestFixture]
+    public class TestMain
+    {
+        [Test]
+        public void foobar()
+        {
+            var fooBytes = File.ReadAllBytes(@"C:\Temp\prop.bin");
+
+            var foo = new PropertyStore(fooBytes);
+            Debug.WriteLine(foo);
+        }
+    }
+}
---
+++ ExtensionBlocks.Test2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionBlocks.Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fooBytes = File.ReadAllBytes(@"C:\Temp\prop.bin");
+
+            var foo = new PropertyStore(fooBytes);
+            Debug.WriteLine(foo);
+        }
+    }
+}
